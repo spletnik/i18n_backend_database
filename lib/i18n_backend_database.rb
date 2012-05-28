1b48814003550009c12c7bd7bd6579ec9faf5478
spemmons <s.p.emmons@gmail.com>
2012-05-28 09:35:20 -0500

    remove diagnostic "puts"

--- lib/i18n_backend_database.rb
+++ lib/i18n_backend_database.rb
@@ -1,4 +1,4 @@
-Dir[File.dirname(__FILE__) + '/../app/**/*.rb'].each{|file| puts "REQUIRE:#{file}"; require file}
+Dir[File.dirname(__FILE__) + '/../app/**/*.rb'].each{|file| require file}
 require File.dirname(__FILE__) + '/i18n_backend_database/database'
 require File.dirname(__FILE__) + '/ext/i18n'
 require File.dirname(__FILE__) + '/i18n_util'
