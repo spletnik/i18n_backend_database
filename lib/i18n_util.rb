50ce195fdfe343ed37f03ac073e78bd5e84b85f6
spemmons <s.p.emmons@gmail.com>
2012-06-20 17:58:50 -0500

    backup an existing translations file in place

--- lib/i18n_util.rb
+++ lib/i18n_util.rb
@@ -251,7 +251,7 @@ class I18nUtil
       puts "#{translations.length - blank_count} EXPORTED..." if verbose?
       puts "WARNING: #{blank_count} BLANKS FOUND!" if verbose?
 
-
+      `cp #{full_path} #{full_path}.bak-#{Time.now.strftime('%Y%m%d%H%M%S')}` if File.exists?(full_path)
       File.open(full_path,'w'){|file| file.write exports.to_yaml}
     end
 
