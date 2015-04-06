7354120fff08ecfa699d1fea9be5ff73cf939876
Vladan Markovic <vladanm@gmail.com>
2015-04-06 15:55:18 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  unless Rails.env.production?
-    resources :locales do
-      resources :translations
-    end
+  resources :locales do
     resources :translations
-    resources :asset_translations
-    #get '/translations' => 'translations#translations', :as => 'translations'
-    #get '/asset_translations' => 'translations#asset_translations', :as => 'asset_translations'
   end
+  resources :translations
+  resources :asset_translations
+  #get '/translations' => 'translations#translations', :as => 'translations'
+  #get '/asset_translations' => 'translations#asset_translations', :as => 'asset_translations'
 end
--- lib/i18n_backend_database/version.rb
+++ lib/i18n_backend_database/version.rb
@@ -1,3 +1,3 @@
 module I18nBackendDatabase
-  VERSION = '0.2.2'
+  VERSION = '0.2.3'
 end
